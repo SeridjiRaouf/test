5dd1c108085063dd7265beb612dea209040bd47b
ali Sriti <alisriti@gmail.com>
2021-05-02 12:27:22 +0100

    Notice Manager Notice mapper

---
+++ NoticeConverter.Core/Managers/Notices/NoticeManager.cs
@@ -0,0 +1,23 @@
+﻿using NoticeConverter.Core.Models.Notices;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace NoticeConverter.Core.Managers.Notices
+{
+    public class NoticeManager : INoticeManager
+    {
+        private const string connectionString = "Data Source=laptop\\mssql;Initial Catalog=NoticeConverter;Integrated Security=True";
+
+        public List<Notice> SelectAllNotices()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM NOTICES", connection);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            connection.Open();
+            da.Fill(dt);
+            return dt.ToNotices();
+        }
+    }
+}
---
+++ NoticeConverter.Core/Models/Notices/NoticeMapper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace NoticeConverter.Core.Models.Notices
+{
+    public static class NoticeMapper
+    {
+        public static Notice ToNotice(this DataRow row)
+        {
+            return new Notice()
+            {
+                Cote = (string)row["Cote"],
+                ISBN = (string)row["ISBN"],
+                Titre = (string)row["Titre"],
+                Auteur = (string)row["Auteur"],
+                Pages = (int)row["Pages"]
+            };
+        }
+
+        public static List<Notice> ToNotices(this DataTable table)
+        {
+            List<Notice> liste = new List<Notice>();
+            foreach (DataRow row in table.Rows)
+            {
+                liste.Add(row.ToNotice());
+            }
+
+            return liste;
+        }
+    }
+}
