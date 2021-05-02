2b0f0cb4d9ed838972ccc66ff6d7c9d4a9b9f18f
ali Sriti <alisriti@gmail.com>
2021-05-02 12:30:34 +0100

    INoticeService

---
+++ NoticeConverter.Core/Services/Notices/INoticeService.cs
@@ -0,0 +1,10 @@
+﻿using NoticeConverter.Core.Models.Notices;
+using System.Collections.Generic;
+
+namespace NoticeConverter.Core.Services.Notices
+{
+    public interface INoticeService
+    {
+        List<Notice> GetNotices();
+    }
+}
