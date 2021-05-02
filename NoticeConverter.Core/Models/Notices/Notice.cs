48abf3ae338a800f50a848dcdb70733457246974
ali Sriti <alisriti@gmail.com>
2021-05-02 12:00:01 +0100

    Model Notice

---
+++ NoticeConverter.Core/Models/Notices/Notice.cs
@@ -0,0 +1,11 @@
+﻿namespace NoticeConverter.Core.Models.Notices
+{
+    public class Notice
+    {
+        public string Cote { get; set; }
+        public string ISBN { get; set; }
+        public string Titre { get; set; }
+        public string Auteur { get; set; }
+        public int Pages { get; set; }
+    }
+}
